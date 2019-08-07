e6e83c64d86df053dcb2c814f30f1cbe0d4c226f
johnnylee24 <33878989+johnnylee24@users.noreply.github.com>
2019-08-07 07:36:21 -0400

    Update modules.py

--- modules.py
+++ modules.py
@@ -17,8 +17,8 @@ def request_nasa_api(API_TOKEN):
     Links = urllib.request.urlopen(request_url, context = this_context)
     reader = Links.read()
     responimg = json.loads(reader.decode('utf-8'))
-    return render_template('index.html', Pic = responimg['url'])
-    webbrower.open(request_url) 
+#     return render_template('index.html', Pic = responimg['url'])
+    webbrowser.get('safari').open_new(request_url) 
     
 
     
